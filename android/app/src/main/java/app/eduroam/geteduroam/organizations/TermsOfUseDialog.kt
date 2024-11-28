bdf8f332ca2aa8dbd6525ffde3447051144b8514
Dániel Zolnai <daniel@egeniq.com>
2024-11-28 11:40:13 +0100

    GETEDUROAM-64: Add content scrim when scrolling in term of use dialog

--- android/app/src/main/java/app/eduroam/geteduroam/organizations/TermsOfUseDialog.kt
+++ android/app/src/main/java/app/eduroam/geteduroam/organizations/TermsOfUseDialog.kt
@@ -1,9 +1,13 @@
 package app.eduroam.geteduroam.organizations
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,7 +18,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -46,17 +55,52 @@ fun TermsOfUseDialog(
             providerInfo?.termsOfUse?.let { termsOfUse ->
                 dialogDescription += "\n\n" + termsOfUse.trim()
             }
-            Column(
-                modifier = Modifier
-                    .heightIn(min = 0.dp, max = 400.dp)
-                    .verticalScroll(state = rememberScrollState())
-            ) {
-                LinkifyText(
-                    text = dialogDescription,
-                    color = MaterialTheme.colorScheme.secondary,
-                    linkColor = MaterialTheme.colorScheme.secondary,
-                    style = MaterialTheme.typography.bodyMedium
-                )
+            val topScrimAmount = 24.dp
+            val bottomScrimAmount = 92.dp
+            Box {
+                Column(
+                    modifier = Modifier
+                        .heightIn(min = 0.dp, max = 400.dp)
+                        .verticalScroll(state = rememberScrollState())
+                        .padding(top = topScrimAmount, bottom = bottomScrimAmount)
+                ) {
+                    LinkifyText(
+                        text = dialogDescription,
+                        color = MaterialTheme.colorScheme.secondary,
+                        linkColor = MaterialTheme.colorScheme.secondary,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+                // Top scrim
+                Surface(
+                    modifier = Modifier.fillMaxWidth()
+                        .align(Alignment.TopCenter)
+                        .height(topScrimAmount)
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    MaterialTheme.colorScheme.surface,
+                                    MaterialTheme.colorScheme.surface.copy(alpha = 0f)
+                                )
+                            )
+                        ),
+                    color = Color.Transparent
+                ) {}
+                // Bottom scrim
+                Surface(
+                    modifier = Modifier.fillMaxWidth()
+                        .align(Alignment.BottomCenter)
+                        .height(bottomScrimAmount)
+                        .background(
+                            brush = Brush.verticalGradient(
+                            colors = listOf(
+                                MaterialTheme.colorScheme.surface.copy(alpha = 0f),
+                                MaterialTheme.colorScheme.surface
+                            )
+                        )
+                    ),
+                    color = Color.Transparent
+                ) {}
             }
             Spacer(modifier = Modifier.size(16.dp))
             Row(
